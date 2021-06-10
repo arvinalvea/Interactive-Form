34add7c7a6d33bfd4983f4bbc4d41e09ddc51c15
Arvin Alvea <arvinalvea@gmail.com>
2021-06-09 20:51:58 -0400

    Update script.js
    Focus on Name field on load.
    Hide Other Job Role and toggle when chosen from Job Role dropdown.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const nameField = document.getElementById("name");
+const otherJobRole = document.getElementById("other-job-role");
+const jobRole = document.getElementById("title");
+
+nameField.focus();
+otherJobRole.style.visibility = "hidden";
+jobRole.addEventListener("change", (e) => {
+    if (e.target.value === "other") {
+        otherJobRole.style.visibility = "visible";
+    }
+});
