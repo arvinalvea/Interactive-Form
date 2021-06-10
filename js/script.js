196e8971e054aaa0f9e818b6e823a74c9d61587b
Arvin Alvea <arvinalvea@gmail.com>
2021-06-09 20:53:33 -0400

    Update script.js
    Hide otherJobRole when Other is not selected.

--- js/script.js
+++ js/script.js
@@ -7,5 +7,7 @@ otherJobRole.style.visibility = "hidden";
 jobRole.addEventListener("change", (e) => {
     if (e.target.value === "other") {
         otherJobRole.style.visibility = "visible";
+    } else {
+        otherJobRole.style.visibility = "hidden";
     }
 });
